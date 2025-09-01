717f09e70a5d16cb38df4fc3c2723b496c42086a
ShumFX <alfadnepr@gmail.com>
2025-09-01 19:34:47 +0300

    finish2

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,9 +1,29 @@
-body {
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-    Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-  margin: 0;
-  background-color: #f8f9fa;
+import { QueryClient, HydrationBoundary, dehydrate } from "@tanstack/react-query";
+import { fetchNotes } from "@/lib/api";
+import NotesClient from "./Notes.client";
+import { NoteTag } from "@/types/note";
+
+
+type Props = {
+    params: Promise<{ slug: string[] }>
+};
+
+export default async function Notes({ params }: Props) {
+     const { slug } = await params;
+  const queryClient = new QueryClient();
+  const tag = slug[0] === "All" ? undefined : (slug[0] as NoteTag);
+
+    await queryClient.prefetchQuery({
+      queryKey:  ["notes", { page: 1, search: "", perPage: 12, tag }],
+      queryFn: () => fetchNotes(1, "", 12, tag),
+        
+      });
+    
+      return (
+        <HydrationBoundary state={dehydrate(queryClient)}>
+          <NotesClient tag = {tag} />
+        </HydrationBoundary>
+    );
+    
+    
 }
