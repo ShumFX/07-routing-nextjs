a67f43187bc17ae4b6cbbc585f05e3b4c4294114
ShumFX <alfadnepr@gmail.com>
2025-09-01 19:15:54 +0300

    work2

--- app/notes/filter/[...slug]/Notes.client.tsx
+++ app/notes/filter/[...slug]/Notes.client.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import css from "@/app/notes/NotesPage.module.css"
+import css from "@/app/notes/filter/[...slug]/NotesPage.module.css"
 
 
 import { useQuery, keepPreviousData } from '@tanstack/react-query'
@@ -12,10 +12,15 @@ import SearchBox from '@/components/SearchBox/SearchBox';
 import Modal from "@/components/Modal/Modal";
 import NoteForm from "@/components/NoteForm/NoteForm";
 import { useDebounce } from "use-debounce";
+import { NoteTag } from "@/types/note";
 
+type NotesClientProps = {
+  tag?: NoteTag;
+};
 
+export default function NotesClient({tag}: NotesClientProps) {
 
-export default function NotesClient() {
+  const perPage = 12;
 
   const [search, setSearch] = useState(""); // стан для пошуку
   const [page, setPage] = useState(1); // стан для пагінації
@@ -23,8 +28,8 @@ export default function NotesClient() {
   const [debouncedSearch] = useDebounce(search, 1000); // стан затримки пошуку
 
   const { data, isSuccess} = useQuery({ // стан запиту
-    queryKey: ["notes", page, debouncedSearch],
-    queryFn: () => fetchNotes(page, debouncedSearch),
+    queryKey: ["notes", page, debouncedSearch, tag],
+    queryFn: () => fetchNotes(page, debouncedSearch, perPage, tag),
     placeholderData: keepPreviousData, // без блимання
     refetchOnMount: false,
   })
